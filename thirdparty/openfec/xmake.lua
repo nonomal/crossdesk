5decc4b0074d225684592b0e32a4b5ad095d1e58
dijunkun <junkun.di@hotmail.com>
2024-05-22 16:30:52 +0800

    Add description and version information for openfec package

--- thirdparty/openfec/xmake.lua
+++ thirdparty/openfec/xmake.lua
@@ -1,7 +1,9 @@
 package("openfec")
 
-    set_homepage("http://openfec.org/")
+    set_homepage("http://openfec.inrialpes.fr/")
+    set_description("Application-Level Forward Erasure Correction codes.")
     set_license("CeCCIL-C")
+    add_versions("1.4.2", "bd1cf0fe466fb7a2ed5c3bcd8840c23dc491dbccfc8e1332989f228f8fb4ec04")
 
     set_sourcedir(os.scriptdir())
     on_install(function (package)
